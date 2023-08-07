a61302ba7e67c11c268e747c3d8dc633c89afa49
minhtri6179 <minhtri.ngt@gmail.com>
2023-08-07 23:50:54 +0700

    part 1

---
+++ app/app/page.tsx
@@ -0,0 +1,21 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button>Like</button>
+    </div>
+  );
+
+  )
+}

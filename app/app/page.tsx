e0494fe9a405fd00d6f92170b5ad0320f99e9c22
minhtri6179 <minhtri.ngt@gmail.com>
2023-10-25 23:41:54 +0700

    change main page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,13 +1,22 @@
-import styles from "./page.module.css";
+"use client";
 
+import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
+          
+
         </p>
+        <Button variant="contained" onClick={() => router.push("/test1")}>TEST1</Button>
+
       </div>
     </main>
   );

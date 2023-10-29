7cb9de807bf17282ba39aa4995ef0eead6c9ec05
minhtri6179 <minhtri.ngt@gmail.com>
2023-10-29 23:56:54 +0700

    change UI api

--- app/app/components/ListeningQuestion3_4.tsx
+++ app/app/components/ListeningQuestion3_4.tsx
@@ -3,8 +3,8 @@ import QuestionsPart2 from "./Question";
 import { get } from "http";
 
 interface ImageGalleryProps {
-  index: number;
-  numofQuestion: number;
+  name_part: string;
+  
 }
 
 interface QuestionData {
@@ -15,29 +15,30 @@ interface AnswerData {
   options: string[];
 }
 
-function getQuestions(from : number, to : number) {
-  return fetch("http://localhost:8080/questions", {
+function getQuestions(name: string) {
+  const url = `http://localhost:8080/questions/${name}`;
+  return fetch(url, {
     method: "GET",
   })
     .then((response) => response.json())
     .then((data) => {
       // Extract the 'question_name' property from the API response and create an array of questions
-      const questions: QuestionData[] = data.slice(from, to).map((item: any) => ({ // Use the appropriate type for 'item'
+      const questions: QuestionData[] = data.map((item: any) => ({ // Use the appropriate type for 'item'
         question: item.question_text,
       }));
       return questions;
     });
 }
-function getAnsers(from : number, to : number) {
-  return fetch("http://localhost:8080/answers", {
+function getAnsers(name_part:string) {
+  const url = `http://localhost:8080/answers/${name_part}`;
+  return fetch(url, {
     method: "GET",
   })
     .then((response) => response.json())
     .then((data) => {
       const answers: AnswerData[] = [];
-      const slicedData = data.slice(from*4, to*4);
-      console.log(slicedData);
-
+      //const slicedData = data.slice(from*4, to*4);
+      const slicedData = data.slice(0, 4);
       for (let i = 0; i < slicedData.length; i+=4) {
         answers.push({
           options: [`A. ${slicedData[i].answer_text}`, `B. ${slicedData[i+1].answer_text}`, `C. ${slicedData[i+2].answer_text}`, `D. ${slicedData[i+3].answer_text}`],
@@ -49,19 +50,20 @@ function getAnsers(from : number, to : number) {
     
 
 
-const ListeningQuestionTest34: React.FC<ImageGalleryProps> = ({ index, numofQuestion }) => {
+const ListeningQuestionTest34: React.FC<ImageGalleryProps> = ({ name_part }) => {
   
   const [questions, setQuestions] = useState<QuestionData[]>([]);
   const [answers, setAnsers] = useState<AnswerData[]>([]);
+  
   useEffect(() => {
-    getQuestions(index, index+numofQuestion)
+    getQuestions(name_part)
       .then((data) => {
         setQuestions(data);
       })
       .catch((error) => {
         console.error(error);
       });
-    getAnsers(index, index+numofQuestion)
+    getAnsers(name_part)
       .then((data) => {
         setAnsers(data);
       })

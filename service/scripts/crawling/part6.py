7468248fbda2285d309ca6e068d8a84fd08ad9a7
minhtri6179 <minhtri.ngt@gmail.com>
2023-11-06 00:34:33 +0700

    part6

--- service/scripts/crawling/part6.py
+++ service/scripts/crawling/part6.py
@@ -1,13 +1,14 @@
-test1part7 = driver.find_element(
-    by=By.XPATH, value="//*[@id='partcontent-9779']/div[2]"
+test1part6 = driver.find_element(
+    by=By.XPATH, value="//*[@id='partcontent-9778']/div[2]"
 )
-children7 = test1part7.find_elements(By.XPATH, "*")
+children6 = test1part6.find_elements(By.XPATH, "*")
+
 res = []
-for block in children7:
+for block in children6:
     res.append(block.text)
 
 
-def part7():
+def part6():
     data = {}
     idx = []
     question = []
@@ -16,21 +17,19 @@ def part7():
     for group in res:
         curGroup = group.split("\n")
         curlen = len(curGroup)
-        rows = curlen // 6
+        rows = curlen // 5
         for i in range(rows):
-            idx.append(curGroup[6 * i])
-            question.append(curGroup[6 * i + 1])
+            idx.append(curGroup[5 * i])
             for j in range(4):
                 if j != 0:
                     idx.append("")
                     question.append("")
 
-                answers.append(curGroup[2 + 6 * i + j])
+                answers.append(curGroup[1 + 5 * i + j])
 
                 nu.append("")
     data = {
         "Question_id": idx,
-        "Question_text": question,
         "Test_name(ETS23-Test1)": nu,
         "Answer_id": nu,
         "Answer_text": answers,
@@ -38,4 +37,4 @@ def part7():
     }
     df = pd.DataFrame(data).reset_index(drop=True)
     df
-    df.to_csv("part7.csv", index=False)
+    df.to_csv("part6.csv", index=False)
